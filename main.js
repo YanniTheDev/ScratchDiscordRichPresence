9d4c28ffa03d6382bb1c3440bfcab8aeb06c6bb8
YanniTheDev <71956500+YanniTheDev@users.noreply.github.com>
2024-12-20 21:21:47 +1300

    rpc should work...

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
-console.log("On Scratch, redirecting....");
-window.open("google.com", "_blank");
+const RPC = require("discord-rpc");
+const client = new RPC.Client({transport: "ipc"});
+
+require('dotenv').config();
+
+const activity = {
+    details: "Working on a Scratch project!",
+    assets: {
+        large_image: "/Images/icon_48.png",
+        large_text: "Scratch",
+        small_image: "/Images/icon_48.png"
+    },
+    timestamps: {start: Date.now()},
+    instance: true
+};
+
+client.on("ready", () => {
+    client.request("SET_ACTIVITY", {pid: process.pid, activity: activity});
+    console.log("Scratch Discord RPC connected");
+});
+
+client.login({clientId: process.env.CLIENT_ID});
