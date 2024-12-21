2331c5197e3b2dad917fc2c8d830c14295d436f9
YanniTheDev <71956500+YanniTheDev@users.noreply.github.com>
2024-12-21 16:52:08 +1300

    i have no idea now

--- scratch-discord-rich-presence/config/webpack.config.js
+++ scratch-discord-rich-presence/config/webpack.config.js
@@ -6,6 +6,7 @@ const common = require('./webpack.common.js');
 const PATHS = require('./paths');
 
 const webpack = require("webpack");
+const { resolve } = require('../build/contentScript.js');
 
 // Merge webpack configuration files
 const config = (env, argv) =>
@@ -18,18 +19,26 @@ const config = (env, argv) =>
     devtool: argv.mode === 'production' ? false : 'source-map',
     resolve: {
       fallback: { 
-        "path": false,
-        "os": false,
-        "crypto": false,
-        "timers":false,
-        "buffer": false,
-        "stream": false,
-        "process": false,
+        "path": require.resolve("path-browserify"),
+        "os": require.resolve("os-browserify/browser"),
+        "crypto": require.resolve("crypto-browserify"),
+        "timers": require.resolve('timers-browserify'),
+        "buffer": require.resolve("buffer/"),
+        "stream": require.resolve("stream-browserify"),
+        "process": require.resolve("process/browser"),
+        "net": require.resolve("net-browserify")
+      },
+      alias: {
+        path: "path-webpack"
       }
     },
+    target: "node",
     plugins: [
       new webpack.ProvidePlugin({
-        process: 'process/browser',
+        process: 'process/browser'
+      }),
+      new webpack.DefinePlugin({
+        "global": {}
       })
     ]
   });
