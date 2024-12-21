b5fea10857290ef1e0df30162484623ca4f6b76e
YanniTheDev <71956500+YanniTheDev@users.noreply.github.com>
2024-12-21 14:42:08 +1300

    restarted :sob:

--- scratch-discord-rich-presence/src/contentScript.js
+++ scratch-discord-rich-presence/src/contentScript.js
@@ -1,32 +1,43 @@
-const RPC = require("discord-rpc");
-const client = new RPC.Client(
-    {
-        transport: "ipc"
-    }
+'use strict';
+
+// Content script file will run in the context of web page.
+// With content script you can manipulate the web pages using
+// Document Object Model (DOM).
+// You can also pass information to the parent extension.
+
+// We execute this script by making an entry in manifest.json file
+// under `content_scripts` property
+
+// For more information on Content Scripts,
+// See https://developer.chrome.com/extensions/content_scripts
+
+// Log `title` of current active web page
+const pageTitle = document.head.getElementsByTagName('title')[0].innerHTML;
+console.log(
+  `Page title is: '${pageTitle}' - evaluated by Chrome extension's 'contentScript.js' file`
 );
 
-require('dotenv').config();
-
-const activity = {
-    details: "Working on a Scratch project!",
-    assets: {
-        large_image: "/Images/icon_48.png",
-        large_text: "Scratch",
-        small_image: "/Images/icon_48.png"
+// Communicate with background file by sending a message
+chrome.runtime.sendMessage(
+  {
+    type: 'GREETINGS',
+    payload: {
+      message: 'Hello, my name is Con. I am from ContentScript.',
     },
-    timestamps: {start: Date.now()},
-    instance: true
-};
+  },
+  (response) => {
+    console.log(response.message);
+  }
+);
 
-client.on("ready", () => {
-    client.request("SET_ACTIVITY", {
-        pid: process.pid, 
-        activity: activity
-    });
+// Listen for message
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.type === 'COUNT') {
+    console.log(`Current count is ${request.payload.count}`);
+  }
 
-    console.log("Scratch Discord RPC connected");
-});
-
-client.login({
-    clientId: process.env.CLIENT_ID
+  // Send an empty response
+  // See https://github.com/mozilla/webextension-polyfill/issues/130#issuecomment-531531890
+  sendResponse({});
+  return true;
 });
