df353d9cb48f690cc55d535841a3f7d58ebd8e23
YanniTheDev <71956500+YanniTheDev@users.noreply.github.com>
2024-12-25 18:59:48 +1300

    completed! :tada:

--- main.js
+++ main.js
@@ -12,7 +12,6 @@ chrome.runtime.sendMessage(extensionID, {mode: 'passive'}, function(response) {
 
 chrome.runtime.onMessage.addListener(function(info, sender, sendResponse) {
     console.log('Presence requested', info);
-    console.log(sender, sendResponse);
     sendResponse({
       clientId: clientID,
       presence: {
